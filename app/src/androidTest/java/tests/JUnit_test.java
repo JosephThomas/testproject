5a8ccf67beb497acac5a055630260b5ee16ff75c
Joseph Thomas <joseph.thomas.c@gmail.com>
2016-04-13 12:28:14 -0700

    test_CI2

--- app/src/androidTest/java/tests/JUnit_test.java
+++ app/src/androidTest/java/tests/JUnit_test.java
@@ -1,6 +1,18 @@
 package tests;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.joseph.testproject3.MainActivity;
@@ -18,13 +30,28 @@ public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity> {
 
     public void test_first()
     {
+        EditText firstNumber;
+        EditText secondNumber;
+        TextView addResult;
+        Button btnAdd;
+
+        double num1,num2,sum;
         mainActivity = getActivity();
 
-        TextView textView = (TextView) mainActivity.findViewById( R.id.tv1 );
-        String tester = textView.getText().toString();
+        //TextView textView = (TextView) mainActivity.findViewById( R.id.tv1 );
+        //String tester = textView.getText().toString();
 
-        assertEquals("Hello World!", tester);
+        //assertEquals("Hello World!", tester);
+
+        firstNumber = (EditText)mainActivity.findViewById(R.id.firstone);
+        secondNumber = (EditText)mainActivity.findViewById(R.id.editText);
+        firstNumber.setText("15");
+        secondNumber.setText("16");
+        addResult = (TextView)mainActivity.findViewById(R.id.textView4);
+        btnAdd = (Button)mainActivity.findViewById(R.id.button);
+        btnAdd.performClick();
+
+        assertEquals( 31, Double.parseDouble(addResult.getText().toString()) );
     }
-
 
 }
--- app/src/main/java/com/example/joseph/testproject3/MainActivity.java
+++ app/src/main/java/com/example/joseph/testproject3/MainActivity.java
@@ -8,8 +8,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText firstNumber;
+    EditText secondNumber;
+    TextView addResult;
+    Button btnAdd;
+
+    double num1,num2,sum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        firstNumber = (EditText)findViewById(R.id.firstone);
+        secondNumber = (EditText)findViewById(R.id.editText);
+        addResult = (TextView)findViewById(R.id.textView4);
+        btnAdd = (Button)findViewById(R.id.button);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                num1 = Double.parseDouble(firstNumber.getText().toString());
+                num2 = Double.parseDouble(secondNumber.getText().toString());
+                sum = num1 + num2;
+                addResult.setText(Double.toString(sum));
             }
         });
     }
