8a2901b282e24ed3871c517039c2368570e4dff5
Joseph Thomas <joseph.thomas.c@gmail.com>
2016-04-13 11:06:35 -0700

    first

---
+++ app/src/androidTest/java/tests/JUnit_test.java
@@ -0,0 +1,30 @@
+package tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.example.joseph.testproject3.MainActivity;
+import com.example.joseph.testproject3.R;
+
+/**
+ * Created by Joseph on 4/12/16.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity> {
+    MainActivity mainActivity;
+    public JUnit_test()
+    {
+        super(MainActivity.class);
+    }
+
+    public void test_first()
+    {
+        mainActivity = getActivity();
+
+        TextView textView = (TextView) mainActivity.findViewById( R.id.tv1 );
+        String tester = textView.getText().toString();
+
+        assertEquals("Hello World !", tester);
+    }
+
+
+}
